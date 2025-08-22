79ac0a38159b4f1d86f4993b2cf2bfc5f4fd7b2a
Denis Popkov <popkov-den03@mail.ru>
2025-08-22 15:11:48 +0600

    init: project

--- composeApp/src/commonMain/kotlin/com/guesssong/game/theme/TypographySystem.kt
+++ composeApp/src/commonMain/kotlin/com/guesssong/game/theme/TypographySystem.kt
@@ -3,11 +3,11 @@ package com.guesssong.game.theme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import guesssongdrinkinggame.composeapp.generated.resources.MavenPro_Bold
 import guesssongdrinkinggame.composeapp.generated.resources.Res
-import guesssongdrinkinggame.composeapp.generated.resources.Sf_Pro_Medium
 import org.jetbrains.compose.resources.Font
 
 @Composable
 fun SfFontFamily() = FontFamily(
-    Font(Res.font.Sf_Pro_Medium, weight = FontWeight.Bold),
+    Font(Res.font.MavenPro_Bold, weight = FontWeight.Bold),
 )
